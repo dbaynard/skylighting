eb7716147f383ffe9c26c0896363e1a4ee4b8ca8
David Baynard <davidbaynard@gmail.com>
2018-03-07 18:18:01 +0000

    Migrate CSS generation to new module

--- skylighting-core/src/Skylighting/Format/HTML.hs
+++ skylighting-core/src/Skylighting/Format/HTML.hs
@@ -4,8 +4,7 @@ module Skylighting.Format.HTML (
     , styleToCss
     ) where
 
-import Data.List (intersperse, sort)
-import qualified Data.Map as Map
+import Data.List (intersperse)
 import Data.Monoid ((<>))
 import Data.String (fromString)
 import qualified Data.Text as Text
@@ -14,11 +13,8 @@ import Skylighting.Types
 import Text.Blaze.Html
 import qualified Text.Blaze.Html5 as H
 import qualified Text.Blaze.Html5.Attributes as A
-import Clay (Css)
-import qualified Clay as C
+import Skylighting.Format.HTML.CSS
 import qualified Clay.Render as C
-import qualified Clay.Media as CM
-import qualified Clay.Text as CT
 
 -- | Format tokens using HTML spans inside @code@ tags. For example,
 -- A @KeywordTok@ is rendered as a span with class @kw@.
@@ -110,39 +106,6 @@ tokenToHtml opts (toktype, txt) =
      else sp
    where sp = H.span ! A.class_ (toValue $ short toktype) $ toHtml txt
 
-short :: TokenType -> String
-short KeywordTok        = "kw"
-short DataTypeTok       = "dt"
-short DecValTok         = "dv"
-short BaseNTok          = "bn"
-short FloatTok          = "fl"
-short CharTok           = "ch"
-short StringTok         = "st"
-short CommentTok        = "co"
-short OtherTok          = "ot"
-short AlertTok          = "al"
-short FunctionTok       = "fu"
-short RegionMarkerTok   = "re"
-short ErrorTok          = "er"
-short ConstantTok       = "cn"
-short SpecialCharTok    = "sc"
-short VerbatimStringTok = "vs"
-short SpecialStringTok  = "ss"
-short ImportTok         = "im"
-short DocumentationTok  = "do"
-short AnnotationTok     = "an"
-short CommentVarTok     = "cv"
-short VariableTok       = "va"
-short ControlFlowTok    = "cf"
-short OperatorTok       = "op"
-short BuiltInTok        = "bu"
-short ExtensionTok      = "ex"
-short PreprocessorTok   = "pp"
-short AttributeTok      = "at"
-short InformationTok    = "in"
-short WarningTok        = "wa"
-short NormalTok         = ""
-
 -- | Returns CSS for styling highlighted code according to the given style.
 styleToCss :: Style -> String
 styleToCss = TL.unpack . C.renderWith config [] . styleToCss'
@@ -150,118 +113,3 @@ styleToCss = TL.unpack . C.renderWith config [] . styleToCss'
     config = C.compact
       { C.rbrace = fromString "}\n"
       }
-
-styleToCss' :: Style -> Css
-styleToCss' f = do
-    divspec
-    numberspec
-    colorspec
-    linkspec
-    mapM_ toCss (sort (Map.toList (tokenStyles f)))
-
-   where colorspec = C.div C.# sourceCode C.?
-           case (defaultColor f, backgroundColor f) of
-             (Nothing, Nothing) -> pure ()
-             (Just c, Nothing)  -> do
-               C.color $ fromColor c
-             (Nothing, Just c)  -> do
-               C.backgroundColor $ fromColor c
-             (Just c1, Just c2) -> do
-               C.color $ fromColor c1
-               C.backgroundColor $ fromColor c2
-
-         numberspec = do
-          C.pre C.# numberSource C.? do
-            C.marginLeft . C.em $ 3
-            pure () `maybe` (\c -> C.borderLeft C.solid (C.px 1) $ fromColor c) $ lineNumberColor f
-
-            C.paddingLeft . C.px $ 4
-
-            C.a C.# sourceLine C.? do
-              C.position C.relative
-              C.empty C.& do
-                C.position C.absolute
-
-              C.before C.& do
-                C.content . C.attrContent . Text.pack $ "data-line-number"
-                C.position C.absolute
-                C.left . C.em $ (-5)
-                C.textAlign . C.alignSide $ C.sideRight
-                C.verticalAlign C.vAlignBaseline
-                C.border C.none C.nil C.none
-                C.pointerEvents C.allEvents
-                C.userSelect C.none
-                C.sym2 C.padding C.nil (C.px 4)
-                C.width . C.em $ 4
-                pure () `maybe` (\c -> C.backgroundColor $ fromColor c) $ lineNumberBackgroundColor f
-                pure () `maybe` (\c -> C.color $ fromColor c) $ lineNumberColor f
-
-         divspec = do
-          C.a C.# sourceLine C.? do
-            C.display C.inlineBlock
-            C.lineHeight . C.unitless $ 1.25
-            C.pointerEvents C.none
-            C.color C.inherit
-            C.textDecoration C.inherit
-
-            C.empty C.& do
-              -- Correct empty line height
-              C.height . C.em $ 1.2
-              C.position C.absolute
-
-          C.star C.# sourceCode C.? do
-            -- Needed for line numbers to be displayed
-            C.overflow C.visible
-
-          -- Collapse neighbours correctly
-          C.div C.# sourceCode C.? do
-            C.sym2 C.margin (C.em 1) C.nil
-          C.pre C.# sourceCode C.? do
-            C.sym C.margin C.nil
-
-          C.code C.# sourceCode C.? do
-            C.whiteSpace CT.pre
-            -- Needed for contents to be position: absolute
-            C.position C.relative
-
-          C.query CM.screen [] $ do
-            C.div C.# sourceCode C.? do
-              C.overflowX C.auto
-
-          C.query CM.print [] $ do
-            C.code C.# sourceCode C.? do
-              C.whiteSpace C.preWrap
-            C.a C.# sourceLine C.? do
-              C.textIndent . C.indent . C.em $ (-1)
-              C.paddingLeft . C.em $ 1
-
-         linkspec = C.query CM.screen [] $ do
-          C.a C.# sourceLine C.# C.before C.? do
-            C.textDecoration C.underline
-
-         numberSource :: C.Refinement
-         numberSource = fromString ".numberSource"
-         sourceCode :: C.Refinement
-         sourceCode = fromString ".sourceCode"
-         sourceLine :: C.Refinement
-         sourceLine = fromString ".sourceLine"
-
-toCss :: (TokenType, TokenStyle) -> Css
-toCss (t,tf) = do
-        C.code C.** C.span C.# shortR t C.? do
-            colorspec
-            backgroundspec
-            weightspec
-            stylespec
-            decorationspec
-            -- C.comment $ showTokenType t
-  where colorspec = pure () `maybe` (\col -> C.color $ fromColor col) $ tokenColor tf
-        backgroundspec = pure () `maybe` (\col -> C.backgroundColor $ fromColor col) $ tokenBackground tf
-        weightspec = if tokenBold tf then C.fontWeight C.bold else pure ()
-        stylespec  = if tokenItalic tf then C.fontStyle C.italic else pure ()
-        decorationspec = if tokenUnderline tf then C.textDecoration C.underline else pure ()
-        shortR = C.byClass . Text.pack . short
-        -- showTokenType t' = case reverse (show t') of
-        --                      'k':'o':'T':xs -> reverse xs
-        --                      _              -> ""
-
---
+++ skylighting-core/src/Skylighting/Format/HTML/CSS.hs
@@ -0,0 +1,168 @@
+{-# LANGUAGE
+    OverloadedStrings
+  , NoImplicitPrelude
+  #-}
+
+module Skylighting.Format.HTML.CSS
+    ( module Skylighting.Format.HTML.CSS
+    ) where
+
+import Prelude hiding (div, span, (**))
+import Clay (Css)
+import Clay hiding (backgroundColor)
+import qualified Clay as C
+import qualified Clay.Media as CM
+import qualified Clay.Text as CT
+import Data.List (sort)
+import qualified Data.Map as Map
+import Data.Text (Text)
+
+import Skylighting.Types
+
+styleToCss' :: Style -> Css
+styleToCss' f = do
+    divspec
+    numberspec
+    colorspec
+    linkspec
+    mapM_ toCss (sort (Map.toList (tokenStyles f)))
+
+   where colorspec = div # sourceCode ?
+           case (defaultColor f, backgroundColor f) of
+             (Nothing, Nothing) -> pure ()
+             (Just c, Nothing)  -> do
+               color $ fromColor c
+             (Nothing, Just c)  -> do
+               C.backgroundColor $ fromColor c
+             (Just c1, Just c2) -> do
+               color $ fromColor c1
+               C.backgroundColor $ fromColor c2
+
+         numberspec = do
+          pre # numberSource ? do
+            marginLeft . em $ 3
+            pure () `maybe` (\c -> borderLeft solid (px 1) $ fromColor c) $ lineNumberColor f
+
+            paddingLeft . px $ 4
+
+            a # sourceLine ? do
+              position relative
+              empty & do
+                position absolute
+
+              before & do
+                content . attrContent $ "data-line-number"
+                position absolute
+                left . em $ (-5)
+                textAlign . alignSide $ sideRight
+                verticalAlign vAlignBaseline
+                border none nil none
+                pointerEvents allEvents
+                userSelect none
+                sym2 padding nil (px 4)
+                width . em $ 4
+                pure () `maybe` (\c -> C.backgroundColor $ fromColor c) $ lineNumberBackgroundColor f
+                pure () `maybe` (\c -> color $ fromColor c) $ lineNumberColor f
+
+         divspec = do
+          a # sourceLine ? do
+            display inlineBlock
+            lineHeight . unitless $ 1.25
+            pointerEvents none
+            color inherit
+            textDecoration inherit
+
+            empty & do
+              -- Correct empty line height
+              height . em $ 1.2
+              position absolute
+
+          star # sourceCode ? do
+            -- Needed for line numbers to be displayed
+            overflow visible
+
+          -- Collapse neighbours correctly
+          div # sourceCode ? do
+            sym2 margin (em 1) nil
+          pre # sourceCode ? do
+            sym margin nil
+
+          code # sourceCode ? do
+            whiteSpace CT.pre
+            -- Needed for contents to be position: absolute
+            position relative
+
+          query CM.screen [] $ do
+            div # sourceCode ? do
+              overflowX auto
+
+          query CM.print [] $ do
+            code # sourceCode ? do
+              whiteSpace preWrap
+            a # sourceLine ? do
+              textIndent . indent . em $ (-1)
+              paddingLeft . em $ 1
+
+         linkspec = query CM.screen [] $ do
+          a # sourceLine # before ? do
+            textDecoration underline
+
+         numberSource :: Refinement
+         numberSource = ".numberSource"
+         sourceCode :: Refinement
+         sourceCode = ".sourceCode"
+         sourceLine :: Refinement
+         sourceLine = ".sourceLine"
+
+toCss :: (TokenType, TokenStyle) -> Css
+toCss (t,tf) = do
+        code ** span # shortR t ? do
+            colorspec
+            backgroundspec
+            weightspec
+            stylespec
+            decorationspec
+            -- comment $ showTokenType t
+  where colorspec = pure () `maybe` (color . fromColor) $ tokenColor tf
+        backgroundspec = pure () `maybe` (C.backgroundColor . fromColor) $ tokenBackground tf
+        weightspec = if tokenBold tf then fontWeight bold else pure ()
+        stylespec  = if tokenItalic tf then fontStyle italic else pure ()
+        decorationspec = if tokenUnderline tf then textDecoration underline else pure ()
+        shortR = byClass . short
+        -- showTokenType t' = case reverse (show t') of
+        --                      'k':'o':'T':xs -> reverse xs
+        --                      _              -> ""
+
+short :: TokenType -> Text
+short KeywordTok        = "kw"
+short DataTypeTok       = "dt"
+short DecValTok         = "dv"
+short BaseNTok          = "bn"
+short FloatTok          = "fl"
+short CharTok           = "ch"
+short StringTok         = "st"
+short CommentTok        = "co"
+short OtherTok          = "ot"
+short AlertTok          = "al"
+short FunctionTok       = "fu"
+short RegionMarkerTok   = "re"
+short ErrorTok          = "er"
+short ConstantTok       = "cn"
+short SpecialCharTok    = "sc"
+short VerbatimStringTok = "vs"
+short SpecialStringTok  = "ss"
+short ImportTok         = "im"
+short DocumentationTok  = "do"
+short AnnotationTok     = "an"
+short CommentVarTok     = "cv"
+short VariableTok       = "va"
+short ControlFlowTok    = "cf"
+short OperatorTok       = "op"
+short BuiltInTok        = "bu"
+short ExtensionTok      = "ex"
+short PreprocessorTok   = "pp"
+short AttributeTok      = "at"
+short InformationTok    = "in"
+short WarningTok        = "wa"
+short NormalTok         = ""
+
