f562e6cfbbc3ff4704b130b49b1faf195d9465ec
David Baynard <davidbaynard@gmail.com>
2018-03-08 14:39:17 +0000

    Move to custom setup that bootstraps xml files

--- skylighting/Setup.hs
+++ skylighting/Setup.hs
@@ -1,2 +1,12 @@
 import Distribution.Simple
-main = defaultMain
+import Distribution.Simple.Setup
+import Distribution.Simple.Utils
+import Distribution.Verbosity (normal)
+-- main = defaultMain
+main = defaultMainWithHooks simpleUserHooks
+  { preBuild = \a fs -> do
+      files <- matchFileGlob "../skylighting-core/xml/*.xml"
+      let v = normal `fromFlagOrDefault` buildVerbosity fs
+      rawSystemExit v "skylighting-extract" files
+      preBuild simpleUserHooks a fs
+  }
